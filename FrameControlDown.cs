7819705f63f71d99b07c1e38ea466ab8c75fbf01
s21113 <80453439+s21113@users.noreply.github.com>
2023-01-26 15:38:31 +0100

    Add files via upload

---
+++ FrameControlDown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameControlDown : MonoBehaviour
+{
+    public GameObject frameDoorsDown;
+    public Animator frameDoorsAnimDown;
+    public BoxCollider coll;
+
+    //private bool up, down;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        frameDoorsAnimDown.SetBool("Opened", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "PlayerBody")
+        {
+            print("Kolizja z graczem");
+            frameDoorsAnimDown.SetBool("Opened", true);
+        }
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        frameDoorsAnimDown.SetBool("Opened", false);
+        print("Brak kolizji z graczem");
+    }
+    
+}
---
+++ FrameControlUp.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameControlUp : MonoBehaviour
+{
+    public GameObject frameDoorsUp;
+    public Animator frameDoorsAnimUp;
+    public BoxCollider coll;
+    bool player, elevator;
+    //private bool up, down;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = false;
+        elevator = false;
+        frameDoorsAnimUp.SetBool("Opened", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.gameObject.tag == "PlayerBody")
+        {
+            print("Kolizja z graczem");
+            player = true;
+            frameDoorsAnimUp.SetBool("Opened", true);
+
+        }
+        /*if (other.gameObject.tag == "Elevator")
+        {
+            print("Kolizja z  windą");
+            elevator = true;
+            //frameDoorsAnimUp.SetBool("Opened", true);
+        }
+        Debug.Log("Kolizja gracz: " + player + " Winda: " + elevator);
+        if(elevator==true && player == true)
+        {
+            frameDoorsAnimUp.SetBool("Opened", true);
+            
+        }*/
+
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        frameDoorsAnimUp.SetBool("Opened", false);
+        print("Brak kolizji z graczem");
+        player = false;
+        elevator = false;
+    }
+    void DoorControl(string direction)
+    {
+        frameDoorsAnimUp.SetTrigger(direction);
+    }
+}
+
