ad7f3be74ff1f3757295f25d1e6be3e45d068835
s21113 <80453439+s21113@users.noreply.github.com>
2023-02-04 19:45:28 +0100

    Add files via upload

--- ZombieScript.cs
+++ ZombieScript.cs
@@ -8,19 +8,14 @@ public class ZombieScript : MonoBehaviour
     private Animator zbAnim;
     public AudioSource audioSource;
     public AudioClip clip;
-    private float volume = 0.8f;
-    private float lifeTime = 1.4f;
+    private float volume = 1f;
+    private float lifeTime = 2f;
 
     private Transform straszak;
     private Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        rnd = GetComponent<Renderer>();
-        rnd.enabled = false;
-        zbAnim = gameObject.GetComponentInParent<Animator>();
-        zbAnim.enabled = false;
-
         //straszak = GameObject.FindGameObjectWithTag("Jumpscare").transform;
         player = GameObject.FindGameObjectWithTag("PlayerBody").transform;
         
@@ -28,20 +23,10 @@ public class ZombieScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Vector3 targetPostition = new Vector3(player.position.x,
-                                      transform.position.y,
-                                      player.position.z);
-
         if (other.gameObject.tag == "PlayerBody")
         {
-            
-            transform.parent.LookAt(targetPostition);
             audioSource.PlayOneShot(clip, volume);
-            rnd.enabled=true;
-            zbAnim.enabled = true;
             Destroy(gameObject, lifeTime);
         }
     }
-
-
 }
--- ZombieWalk.cs
+++ ZombieWalk.cs
@@ -1,130 +1,198 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieWalk : MonoBehaviour
 {
     private Rigidbody rb;
-    [Range(0, 8f)] [SerializeField] private float speed = 3f;
-    private Vector3 m_Velocity = Vector3.zero;
     public GameObject WP1;
     public GameObject WP2;
     private GameObject player;
     bool WP1cmp = true;
     bool WP2cmp = false;
     private bool isInRange;
-    Vector3 vecBgn;
     Animator animator;
-    private int hpPoints;
-    private bool freze;
-    private bool inTrigger;
-
+    NavMeshAgent agencik;
+    private int hpPoints = 1;
+    private bool freeze = false;
+    private bool goingToPlayer = false;
+    private bool spinning = false;
+    private float speed;
+    // private Collider coll1;
+    // private Collider coll2;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-        hpPoints = 4;
-        freze = false;
-        isInRange = false;
         animator.SetBool("isInRange", isInRange);
         player = GameObject.FindGameObjectWithTag("PlayerBody");
-        inTrigger = false;
+        agencik = GetComponent<NavMeshAgent>();
 
+        // GameObject x1 = GameObject.FindGameObjectWithTag("WP1");
+        // GameObject x2 = GameObject.FindGameObjectWithTag("WP2");
+        // coll1 = x1.GetComponent<Collider>();
+        // coll2 = x2.GetComponent<Collider>();
 
+        // coll2.enabled = false;
+        speed = UnityEngine.Random.Range(2.5f, 6.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 targetVelocity = new Vector3 (10 * 10f, rb.velocity.x, rb.velocity.x);
-        //rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
         Vector3 myVector = new Vector3(0.0f, 0.0f, 5.0f);
-        rb.velocity = myVector * 111.0f;
+        rb.velocity = myVector * 125.0f;
 
-        //Debug.Log(rb.velocity);
-        //rb.velocity = transform.forward * 5f;
-        lookat();
+        //  Debug.Log(coll2);
     }
+   
 
-    private void lookat()
+    private void FixedUpdate()
     {
         float distance = Vector3.Distance(transform.position, player.transform.position);
         Vector3 targetPostition = new Vector3(player.transform.position.x,
                                       transform.position.y,
                                       player.transform.position.z);
-        Vector3 moveAround1 = new Vector3(WP1.transform.position.x, WP1.transform.position.y, WP1.transform.position.z);
-        Vector3 moveAround2 = new Vector3(WP2.transform.position.x, WP2.transform.position.y, WP2.transform.position.z);
 
-        //bool done = false;
+        if (distance <= 10f && Math.Abs(transform.position.y - player.transform.position.y) <= 1.5f)
+        {
+            goingToPlayer = true;
+            agencik.speed = 8f;
+            agencik.SetDestination(targetPostition);
+            //transform.position = Vector3.MoveTowards(transform.position, targetPostition, speed * 2 * Time.deltaTime);
+            //transform.LookAt(targetPostition);
+        }
+        else
+        {
+            goingToPlayer = false;
+            agencik.speed = speed;
+        }
 
-        if (distance < 3 && !freze)
+        if (distance < 2.8f && !freeze)
         {
             isInRange = true;
             animator.SetBool("isInRange", isInRange);
-            //transform.LookAt(targetPostition);
-        }
-        else if (!freze)
-        {
+            goingToPlayer = false;
 
+            if(!spinning){
+                StartCoroutine(Hit());
+            }
+            
+        }
+        else if (!freeze)
+        {
             isInRange = false;
             animator.SetBool("isInRange", isInRange);
-            if (WP1cmp)
+            if (WP1cmp && !goingToPlayer)
             {
-                transform.position = Vector3.MoveTowards(transform.position, moveAround1, speed * Time.deltaTime);
-
+                agencik.SetDestination(WP1.transform.position);
+                //transform.position = Vector3.MoveTowards(transform.position, WP1.transform.position, speed * Time.deltaTime);
+                transform.LookAt(WP1.transform);
             }
-            else if (WP2cmp)
+            else if (WP2cmp && !goingToPlayer)
             {
-                transform.position = Vector3.MoveTowards(transform.position, moveAround2, speed * Time.deltaTime);
+                agencik.SetDestination(WP2.transform.position);
+                //transform.position = Vector3.MoveTowards(transform.position, WP2.transform.position, speed * Time.deltaTime);
+                transform.LookAt(WP2.transform);
             }
-
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "WP1")
+        
+        if (other.gameObject == WP1 && !WP2cmp)
         {
+            // coll2.enabled = true;
+
             WP1cmp = !WP1cmp;
             WP2cmp = !WP2cmp;
-            transform.Rotate(new Vector3(0, 180, 0), Space.World);
+            transform.Rotate(new Vector3(0, 90, 0), Space.World);
+            // coll1.enabled = false;
         }
-        if (other.gameObject.name == "WP2")
+        if (other.gameObject == WP2 && !WP1cmp)
         {
+            // coll1.enabled = true;
+            
             WP2cmp = !WP2cmp;
             WP1cmp = !WP1cmp;
-            transform.Rotate(new Vector3(0, 180, 0), Space.World);
+            transform.Rotate(new Vector3(0, -90, 0), Space.World);
+            // coll2.enabled = false;
+            
         }
-        if (other.gameObject.tag == "Bambus")
+        
+        
+        if (other.gameObject.CompareTag("Bambus"))
         {
-            hpPoints = hpPoints - 1;
-            if (hpPoints < 0)
+            hpPoints -= 1;
+            if (hpPoints <= 0)
             {
                 Destroy(gameObject);
             }
-
         }
-        
     }
 
-    public void setIsInRangeFalse(){
-        freze = false;
+    public void setIsInRangeFalse()
+    {
+        freeze = false;
         isInRange = false;
         animator.SetBool("isInRange", isInRange);
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "reppelant")
+        // if(other.gameObject.tag != "Untagged" && other.gameObject.tag != "Jumpscare" && other.gameObject.tag == "reppelant" && other.gameObject.tag != "PlayerBody" ){
+        //     Debug.Log(other.gameObject.tag);
+        // }
+        
+        if (other.gameObject.CompareTag("reppelant"))
         {
-            inTrigger = true;
-            freze = true;
+            //Debug.Log("444444444");
+           
+            freeze = true;
+            agencik.isStopped = true;
             isInRange = true;
             animator.SetBool("isInRange", isInRange);
+
+        }else if (spinning) {
+
+            //Debug.Log("5555555");
+            
+            freeze = true;
+            agencik.isStopped = true;
+            isInRange = true;
+            animator.SetBool("isInRange", isInRange);
+
+        }else{
+            
+            freeze = false;
+            agencik.isStopped = false;
+            isInRange = false;
+            animator.SetBool("isInRange", isInRange);
         }
-        
+    }
+
+    IEnumerator Hit (){
+        spinning = true;
+        //Debug.Log("JEB");
+
+        //Debug.Log("STOP");
+        for (int i=0; i<300; i++){
+            transform.Rotate(new Vector3(0, 8, 0), Space.World);
+            yield return new WaitForSeconds(0.01f);
+        }
+      
+
+        //Debug.Log("RETURN");
+
+        spinning = false;
+        StopCoroutine(Hit());
 
     }
+
+
+
 }
